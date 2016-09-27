25a113dc3ec9452bace540c72854c4fac182910b
Nonpawit <nonpawit.tee@gmail.com>
2016-09-28 02:47:02 +0700

    Added Logger

--- JavaSocket/src/main/java/WebSocket/RemoteServer.java
+++ JavaSocket/src/main/java/WebSocket/RemoteServer.java
@@ -6,9 +6,17 @@ import org.java_websocket.server.WebSocketServer;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.net.UnknownHostException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.Collection;
+import java.util.Date;
+import java.util.logging.FileHandler;
+import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
+import java.util.logging.XMLFormatter;
 
 import foo.RobotControl;
 
@@ -17,9 +25,30 @@ import foo.RobotControl;
  */
 public class RemoteServer extends WebSocketServer{
 
+    private Logger log = Logger.getLogger(this.getClass().getName());
     private RobotControl control;
-
     private JSONObject currentProduct;
+    private FileHandler fh;
+
+    private void init() {
+        // "%1$tF %1$tT.%1$tL %4$s, %2$s: %5$s%6$s%n"
+        System.setProperty("java.util.logging.SimpleFormatter.format",
+                "%1$tY-%1$tm-%1$td %1$tH:%1$tM:%1$tS %4$s %2$s %5$s%6$s%n");
+
+        Date date = new Date();
+        String dateString = new SimpleDateFormat("yyMMdd_HHmmss_S").format(date);
+
+        try {
+            XMLFormatter xmlFormatter = new XMLFormatter();
+            fh = new FileHandler("./" + dateString + "-logfile.log");
+            fh.setFormatter(xmlFormatter);
+            log.addHandler(fh);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        log.info("Hello, world");
+    }
 
     public RemoteServer(int port) throws UnknownHostException {
         super(new InetSocketAddress(port));
@@ -27,6 +56,7 @@ public class RemoteServer extends WebSocketServer{
 
     public RemoteServer(int port, RobotControl control) throws UnknownHostException {
         super(new InetSocketAddress(port));
+        init();
         this.control = control;
     }
 
@@ -61,7 +91,7 @@ public class RemoteServer extends WebSocketServer{
             } else if (action.equals("trigger_mode")) {
                 triggerMode(jsonObject);
             } else if (action.equals("tap")) {
-                control.click();
+                click();
             } else if (action.equals("forward_swipe") || action.equals("backward_swipe")) {
                 sendToAll(jsonObject);
             }
@@ -72,9 +102,16 @@ public class RemoteServer extends WebSocketServer{
         }
     }
 
+    private void click() {
+        log.info("Clicked");
+        control.click();
+    }
+
     private void mouseControl(JSONObject jsonObject) throws JSONException {
         int deltaX = jsonObject.getInt("delta_x");
         int deltaY = jsonObject.getInt("delta_y");
+        String message = String.format("Moved : (%d,%d)", deltaX, deltaY);
+        log.info(message);
 
         control.move(deltaX, deltaY);
     }
